a8a3b8e69eedc3e7afdf0844ed087df91d536735
Nicolas Herrera <herreran903@gmail.com>
2024-06-24 19:52:45 -0500

    Add backend and frontend deployment and service configurations

--- frontend/src/api/apiBase.js
+++ frontend/src/api/apiBase.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiURL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/'
+  'http://connection-service:8000'
 
 const apiBase = axios.create({
   baseURL: apiURL,
