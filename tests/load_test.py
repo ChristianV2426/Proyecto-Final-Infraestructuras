09b07c035c35dba6596642693aa0790c34424fd8
Samuel Galindo <itzsaky@outlook.com>
2024-07-08 17:37:20 -0500

    prueba sin errores

--- tests/load_test.py
+++ tests/load_test.py
@@ -1,12 +1,15 @@
 from locust import HttpUser, task, between, SequentialTaskSet
 from datetime import datetime
-
+import random
+import string
 
 class Tasks(SequentialTaskSet):
     @task
     def create_projects(self):
+        caracteres = string.ascii_letters + string.digits
+        string_random = ''.join(random.choice(caracteres) for i in range(5))
         data = {
-            "project_name": f"Proyecto {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+            "project_name": f"Proyecto {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {string_random} ",
             "project_description": "",
             "start_date": "2024-06-10"
         }
